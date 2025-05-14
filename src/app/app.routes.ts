dc9209a6ce981e948c428c18864102b159fbc046
adriann postigo <adriannparanhos@gmail.com>
2025-05-14 14:16:10 -0300

    Created a new component ProductsComponent

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { ProductsComponent } from './components/products/products.component';
 
 export const routes: Routes = [
     {path: 'dashboard', component: DashboardComponent},
+    {path: 'products', component: ProductsComponent},
 ];
