6730c7e6376e1c408a28c893c6eb7c01a63a7f10
adriann postigo <adriannparanhos@gmail.com>
2025-06-27 09:52:24 -0300

    Feat: Adjusted the profile settings section. Added environment variables tab. It is now possible to enter and exit edit mode

--- src/app/pages/settings/settings.component.ts
+++ src/app/pages/settings/settings.component.ts
@@ -5,22 +5,26 @@ import { AuthLoginService } from '../../services/auth-login.service';
 import { LucideAngularModule } from 'lucide-angular';
 import { of } from 'rxjs'; 
 import { EnvironmentVariablesService } from '../../services/fetchs/environment-variables.service';
+import { FormatKeyPipe } from '../../pipes/format-key.pipe';
 
 @Component({
   selector: 'app-settings',
   standalone: true,
-  imports: [CommonModule, ReactiveFormsModule, LucideAngularModule],
+  imports: [CommonModule, ReactiveFormsModule, LucideAngularModule, FormatKeyPipe],
   templateUrl: './settings.component.html',
 })
 export class SettingsComponent implements OnInit {
   profileForm: FormGroup;
   passwordForm: FormGroup;
+  variablesForm!: FormGroup;
 
   lastVariable: any | null = null;
   lastUpdate: Date = new Date(); 
   
   isAdmin = false;
   allUsers: any[] = []; 
+  isEditingVariables: boolean = false;
+  isVisualize: boolean = true;
 
   constructor(
     private fb: FormBuilder, 
@@ -35,6 +39,22 @@ export class SettingsComponent implements OnInit {
       currentPassword: ['', Validators.required],
       newPassword: ['', [Validators.required, Validators.minLength(6)]],
       confirmPassword: ['', Validators.required],
+    });
+
+    this.variablesForm = this.fb.group({
+      valorHoraUsinagem: [null, Validators.required],
+      rebarbacao: [null, Validators.required],
+      valorUnitarioDesenho: [null, Validators.required],
+      markUpRevestimento: [null, Validators.required],
+      descontoUm: [null, Validators.required],
+      descontoDois: [null, Validators.required],
+      descontoTres: [null, Validators.required],
+      descontoQuatro: [null, Validators.required],
+      mark_up_frete: [null, Validators.required],
+      mark_up_black: [null, Validators.required],
+      mark_up_natural: [null, Validators.required],
+      mark_up_ultra: [null, Validators.required],
+      resinValue: [null, Validators.required]
     });
   }
 
@@ -80,7 +100,7 @@ export class SettingsComponent implements OnInit {
   }
 
   deleteUser(userId: number): void {
-    if (confirm(`Tem certeza que deseja excluir o usuário com ID ${userId}?`)) {
+    if (confirm(`Tem certeza que deseja excluir o usuário ${userId}?`)) {
       console.log(`Admin quer DELETAR o usuário com ID: ${userId}`);
       alert(`Simulação de exclusão do usuário ${userId}`);
       this.allUsers = this.allUsers.filter(u => u.id !== userId);
@@ -91,19 +111,60 @@ export class SettingsComponent implements OnInit {
     console.log("Buscando variáveis do sistema do backend...");
     
     this.environmentVariablesService.getVariables().subscribe({
-      next: (data) => {
-        this.lastVariable = data; 
-        console.log("Variáveis carregadas com sucesso:", this.lastVariable);
+      next: (data: any[]) => {
+        if (Array.isArray(data) && data.length > 0) {
+          this.lastVariable = data[data.length - 1]; 
+          console.log("Variáveis carregadas com sucesso:", this.lastVariable);
+        } else {
+          console.warn("Nenhum dado de variável foi retornado pelo backend ou o formato está incorreto.");
+          this.lastVariable = null;
+        }
       },
       error: (err) => {
         console.error("Erro ao carregar as variáveis do sistema", err);
         alert("Não foi possível carregar as variáveis do sistema.");
       }
     });
+    this.isVisualize = true;
   }
 
   editVariables(): void {
-    console.log("Admin quer editar as variáveis do sistema.");
-    alert('Simulação de edição das variáveis.');
+    if (!this.lastVariable) return;
+
+    this.variablesForm.patchValue(this.lastVariable);
+    this.isEditingVariables = true;
+    this.isVisualize = false;
+  }
+
+  cancelEditVariables(): void {
+    this.isEditingVariables = false;
+    this.isVisualize = true
+  }
+
+  onSaveVariables(): void {
+    if (this.variablesForm.invalid) {
+      alert('Por favor, preencha todos os campos de variáveis obrigatórios.');
+      return;
+    }
+
+    const updatedVariables = this.variablesForm.value;
+    console.log("Enviando novas variáveis para o backend:", updatedVariables);
+
+    this.environmentVariablesService.sendVariables(updatedVariables).subscribe({
+      next: (savedData) => {
+        this.lastVariable = savedData;
+        alert('Variáveis salvas com sucesso!');
+        this.isEditingVariables = false;
+        this.loadLastVariable();
+      },
+      error: (err) => {
+        console.error("Erro ao salvar as variáveis", err);
+        alert("Ocorreu um erro ao salvar as variáveis.");
+      }
+    });
+  }
+
+  get variableKeys(): string[] {
+    return Object.keys(this.variablesForm.controls);
   }
 }
---
+++ src/app/pipes/format-key.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { FormatKeyPipe } from './format-key.pipe';
+
+describe('FormatKeyPipe', () => {
+  it('create an instance', () => {
+    const pipe = new FormatKeyPipe();
+    expect(pipe).toBeTruthy();
+  });
+});
