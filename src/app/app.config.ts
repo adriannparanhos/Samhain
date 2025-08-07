a88dbc3637041e9e27c399736342386a30ef4a36
adriann postigo <adriannparanhos@gmail.com>
2025-08-07 15:15:31 -0300

    Feat: Added a calendar system to schedule visits from external vendors. Using Angular's fullCalendar lib

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -5,8 +5,6 @@ import { routes } from './app.routes';
 import { NumericToStringInterceptor } from './services/httpInterceptors/numeric-to-string-interceptor.service';
 import { authInterceptor } from './interceptors/auth.interceptor';
 import { LucideAngularModule, LUCIDE_ICONS, LucideIconProvider, FileBox, Home, Building2, Package, FileText, LogOut, Menu, X, User, Search, Check, Clock, HelpCircle, ArrowLeft, Save, Building2Icon, Eye, Pencil, Trash2, Info, ShoppingCart, FileCheck, UserCircle, Users, FilePenIcon, FilePenLine, KeyRound, SlidersHorizontal, Hammer, UploadCloud, Paperclip, Cone, SquareFunction, Cylinder, Box, Truck, ListCheck, FileSpreadsheet, Wrench, UserRound, Printer, DollarSign, CalendarDays } from 'lucide-angular';
-import { CalendarModule, DateAdapter, CalendarCommonModule } from 'angular-calendar';
-import { adapterFactory } from 'angular-calendar/date-adapters/date-fns';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { EventClickArg } from '@fullcalendar/core/index.js';
 
@@ -19,13 +17,7 @@ export const appConfig: ApplicationConfig = {
     { provide: LUCIDE_ICONS, multi: true, useValue: new LucideIconProvider({ FileBox, Home, Building2, Package, FileText, LogOut, Menu, X, User, Search, Check, Clock, HelpCircle, ArrowLeft, Save, Building2Icon, Eye, Pencil, Trash2, Info, ShoppingCart, FileCheck, UserCircle, Users, FilePenIcon, FilePenLine, KeyRound, SlidersHorizontal, Hammer, UploadCloud, Paperclip, Cone, SquareFunction, Cylinder, Box, Truck, ListCheck, FileSpreadsheet, Wrench, UserRound, Printer, DollarSign, CalendarDays }) },
     { provide: LOCALE_ID, useValue: 'pt-BR' },
     { provide: HTTP_INTERCEPTORS, useClass: NumericToStringInterceptor, multi: true }, 
-    provideAnimations(), 
-    importProvidersFrom(
-      CalendarModule.forRoot({
-        provide: DateAdapter,
-        useFactory: adapterFactory,
-      })
-    ),
+    provideAnimations()
     
   ]
 };
